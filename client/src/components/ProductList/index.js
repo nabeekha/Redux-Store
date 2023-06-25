1ec846e87789b3044288d16a9c955ab7ff856a66
Nabeel Khan <nabeekha@umich.edu>
2023-06-25 15:13:13 -0400

    updated components

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux'; // Import useSelector and useDispatch from 'react-redux'
 import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
